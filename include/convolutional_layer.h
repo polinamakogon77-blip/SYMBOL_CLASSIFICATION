de9b597a8549752bf0bfd5da146d168976d1a0be
Makogon Polina <polinamakogon77@gmail.com>
2026-05-12 22:33:51 +0700

    добавлен градиент для сверточного слоя

--- include/convolutional_layer.h
+++ include/convolutional_layer.h
@@ -5,12 +5,23 @@
 
 
 // реализует сверточный слой
-float *conv_layer(Tensor *tensor, float *ker, int **index, int size_ker, int row_ker, int col_ker, int *pool_row, int *pool_col, float **input_relu);
+float *conv_layer(Tensor *tensor, float *ker, int **index, int size_ker, int row_ker, int col_ker, int *pool_row, int *pool_col, float **input_relu, float **matrix_col);
 // tensor - набор изображений
 // size_ker - размер ядра
 // int row_ker, int col_ker - размер массива весов
 // pool_row, pool_col - указатели на размеры результата
 // index - индексы максисальных элементов
 // input_relu - матрица, которая подается на вход ф-ии активации
+// matrix_col - результат im2col
+
+// вычисляет градиент для сверточного слоя
+void gradient_conv(float *gradient_in, Tensor *tensor, float *weight, float *matrix_col, int ker_size, int cnt_filter, float *grad_weight, \
+    float *grad_bias, float *grad_input);
+// gradient_in - градиент ReLU
+// tensor - вход в im2col
+// weight - веса фильтров
+// matrix_col - результат im2col
+// cnt_filter - к-во фильтров
+// grad_weight, grad_bias, grad_input - градинеты весов, смещения, входа
 
 #endif
--- project_files/convolutional_layer.c
+++ project_files/convolutional_layer.c
@@ -5,12 +5,18 @@
 #include "mult_matrix.h"
 #include "pooling.h"
 #include "relu.h"
+#include "col2im.h"
 
 
-float *conv_layer(Tensor *tensor, float *ker, int **index, int size_ker, int row_ker, int col_ker, int *pool_row, int *pool_col, float **input_relu) {
+float *conv_layer(Tensor *tensor, float *ker, int **index, int size_ker, int row_ker, int col_ker, int *pool_row, int *pool_col, float **input_relu, float **matrix_col) {
 
     int row_res, col_res; // размер результата
     float *matrix = im2col(tensor, size_ker, &row_res, &col_res);
+    
+    // запоминаю результат im2col
+    *matrix_col = (float*)malloc(sizeof(float) * row_res * col_res); 
+    memcpy(*matrix_col, matrix, sizeof(float) * row_res * col_res);
+
     matrix = mult_matrix(matrix, ker, row_res, col_res, row_ker, col_ker);
     // размер для polling
     int feature_h = (tensor->height + 2 - size_ker) + 1;
@@ -49,4 +55,79 @@ float *conv_layer(Tensor *tensor, float *ker, int **index, int size_ker, int row
 
     free(matrix);
     return all_pooled;
+}
+
+void gradient_conv(float *gradient_in, Tensor *tensor, float *weight, float *matrix_col, int ker_size, int cnt_filter, float *grad_weight, \
+    float *grad_bias, float *grad_input) {
+    
+    // размер одного изображения
+    int h = tensor->height;
+    int w = tensor->width;
+    // размер одного изображения с рамкой
+    int pad_h = h + 2;
+    int pad_w = w + 2;
+    // размер одного изображения после im2col с учетом каналов
+    int h_im = tensor->count_channel * ker_size * ker_size; 
+    int w_im = (pad_h - ker_size + 1) * (pad_w - ker_size + 1);
+
+    // градиент для смещения
+    for (int f = 0; f < cnt_filter; ++f) { // по всем фильтрам
+        grad_bias[f] = 0;
+        for (int i = 0; i < tensor->count_picture; ++i) { // по всем изображениям
+            for (int col = 0; col < w_im; ++col) { // по всем столбцам изображения
+                grad_bias[f] += gradient_in[(i * cnt_filter + f) * w_im + col]; // складываем ошибку
+            }
+        }
+        
+    }
+
+    // градниент для весов
+    // grad_weight = gradient_in * matrix_col.T
+    for (int i = 0; i < tensor->count_picture; ++i) { // по всем изображениям
+        for (int f = 0; f < cnt_filter; ++f) { // по всем фильтрам
+            for (int row = 0; row < h_im; ++row) { // по строкам
+                float sum = 0;
+                for (int col = 0; col < w_im; ++col) { // по столбцам
+                    sum += gradient_in[i * cnt_filter * w_im + f * w_im + col] * matrix_col[i * h_im * w_im + row * w_im + col];
+                }  
+                grad_weight[f * h_im + row] += sum;
+            } 
+        }
+    }
+
+    // градиент для входа
+    // grad_col = weight.T * gradient_in
+    float *weight_T = (float *)malloc(sizeof(float) * cnt_filter * h_im);
+    // транспонируем м-цу весов
+    for (int i = 0; i < cnt_filter; ++i) {
+        for (int j = 0; j < h_im; ++j) {
+            weight_T[j * cnt_filter + i] = weight[i * h_im + j];
+        }
+    }
+
+    float *grad_col = (float *)calloc(tensor->count_picture * h_im * w_im, sizeof(float)); // градиент выхода но в виде столбчатой м-цы 
+
+    for (int i = 0; i < tensor->count_picture; ++i) {
+        for (int k = 0; k < h_im; ++k) {
+            for (int p = 0; p < w_im; ++p) {
+                float sum = 0;
+                for (int f = 0; f < cnt_filter; ++f) {
+                    int grad_idx = (i * cnt_filter + f) * w_im + p;
+                    sum += weight_T[k * cnt_filter + f] * gradient_in[grad_idx];
+                }
+                grad_col[i * h_im * w_im + k * w_im + p] = sum;
+            }
+        }
+    }
+
+    // создаем тензор для grad_input
+    Tensor *temp = create_tensor(NULL, tensor->count_picture, tensor->count_channel, h, w);
+    temp->data = (float *)malloc(sizeof(float) * h * w * tensor->count_channel * tensor->count_picture);
+    col2im(grad_col, temp, ker_size);
+    memcpy(grad_input, temp->data, sizeof(float) * h * w * tensor->count_channel * tensor->count_picture);
+    
+    free_tensor(temp);
+    free(grad_col);
+    free(weight_T);
+
 }
