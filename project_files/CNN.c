9f1fa29c462d03f6686d125c37ee5d1a289d6492
Makogon Polina <polinamakogon77@gmail.com>
2026-05-14 15:45:45 +0700

    в СNN добавлены все градиенты

--- project_files/CNN.c
+++ project_files/CNN.c
@@ -58,18 +58,34 @@ float *CNN(Tensor *tensor, float *filter_1, float *filter_2, float *filter_3, fl
     float *gradient_bias_3;
     update_weight(full_conv, conv_2, filter_3, &gradient_out, &gradient_filter3, &gradient_bias_3, tensor->count_picture, row_conv_2 * col_conv_2);
 
-    // градиент для ф-ии пулирования
-    float *gradient_poll;
-    gradient_pooling(gradient_out, index_poll_2, &gradient_poll, tensor->count_picture * row_conv_2 * col_conv_2);    
+    // градиент для ф-ии пулирования_2
+    float *gradient_poll2;
+    gradient_pooling(gradient_out, index_poll_2, &gradient_poll2, tensor->count_picture * row_conv_2 * col_conv_2);    
 
-    //градиент для ф-ии активации
-    float *gradient_relu;
-    gradient_ReLU(gradient_poll, input_relu_2, &gradient_relu, tensor->count_picture * row_conv_2 * col_conv_2 * 4);
+    //градиент для ф-ии активации_2
+    float *gradient_relu2;
+    gradient_ReLU(gradient_poll2, input_relu_2, &gradient_relu2, row_filter_1 * 2 * (tensor_conv->height + 2 - size_ker + 1) * (tensor_conv->width + 2 - size_ker + 1));
 
     // градиент для 2-ого сверточного слоя
-    // float *gradient_cov_2;
-    // float *gradient_bias_2;
-    // gradient_conv(gradient_relu, tensor, filter_2, matrix_col_2, size_ker, row_filter_1 * 2, ); 
+    float *gradient_conv2 = (float *)malloc(sizeof(float) * tensor_conv->count_channel * tensor_conv->count_picture * tensor_conv->height * tensor_conv->width);
+    float *gradient_filter2 = (float *)calloc(tensor_conv->count_channel * size_ker * size_ker, sizeof(float));
+    float *gradient_bias_2 = (float *)calloc(row_filter_1 * 2, sizeof(float));
+    gradient_conv(gradient_relu2, tensor_conv, filter_2, matrix_col_2, size_ker, row_filter_1 * 2, gradient_filter2, gradient_bias_2, gradient_conv2); 
+
+     // градиент для ф-ии пулирования
+    float *gradient_poll1;
+    gradient_pooling(gradient_conv2, index_poll_1, &gradient_poll1, tensor->count_picture * row_conv_1 * col_conv_1);    
+
+    //градиент для ф-ии активации
+    float *gradient_relu1;
+    gradient_ReLU(gradient_poll1, input_relu_1, &gradient_relu1, row_filter_1 * (tensor->height + 2 - size_ker + 1) * (tensor->width + 2 - size_ker + 1));
+
+    // градиент для 1-ого сверточного слоя
+    float *gradient_conv1 = (float *)malloc(sizeof(float) * tensor->count_channel * tensor->count_picture * tensor->height * tensor->width);
+    float *gradient_filter1 = (float *)calloc(tensor->count_channel * size_ker * size_ker, sizeof(float));
+    float *gradient_bias_1 = (float *)calloc(row_filter_1, sizeof(float));
+    gradient_conv(gradient_relu1, tensor, filter_1, matrix_col_1, size_ker, row_filter_1, gradient_filter1, gradient_bias_1, gradient_conv1); 
+
 
     // очистка
     free(conv_1);
@@ -78,12 +94,22 @@ float *CNN(Tensor *tensor, float *filter_1, float *filter_2, float *filter_3, fl
     free(index_poll_1);
     free(index_poll_2);
     free(gradient_out);
-    free(gradient_filter3);
-    free(gradient_bias_3);
-    free(gradient_poll);
-    free(gradient_relu);
+    free(gradient_poll1);
+    free(gradient_relu1);
+    free(gradient_poll2);
+    free(gradient_relu2);
     free(input_relu_1);
     free(input_relu_2);
+    free(gradient_conv1);
+    free(gradient_conv2);
+
+    // временно
+    free(gradient_bias_1);
+    free(gradient_bias_2);
+    free(gradient_bias_3);
+    free(gradient_filter1);
+    free(gradient_filter2);
+    free(gradient_filter3);
 
     return full_conv;
 }
